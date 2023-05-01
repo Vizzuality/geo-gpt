91e26377cb4493bd0969132aef4e3153537ff26f
CV-Gate <miguel.mendoza@vizzuality.com>
2023-05-01 13:53:28 +0200

    removes unused code

--- blueprints/geocode.py
+++ blueprints/geocode.py
@@ -8,27 +8,10 @@ geocode_bp = Blueprint('geocode', __name__)
 
 geolocator = Nominatim(user_agent='geo-gpt')
 
-def sec(x):
-    return(1/cos(x))
-
-
-def latlon_to_xyz(lat, lon, z):
-    tile_count = pow(2, z)
-    x = (lon + 180) / 360
-    y = (1 - log(tan(radians(lat)) + sec(radians(lat))) / pi) / 2
-    return(tile_count*x, tile_count*y)
-
-def bbox_to_xyz(min_lon, max_lon, min_lat, max_lat, z):
-    x_min, y_max = latlon_to_xyz(min_lat, min_lon, z)
-    x_max, y_min = latlon_to_xyz(max_lat, max_lon, z)
-    return(floor(x_min), floor(x_max),
-           floor(y_min), floor(y_max))
-
-
 def get_bbox(place, size=10):
     location = geolocator.geocode(place)
     if not location:
-        return None
+        return None 
     lat, lon = location.latitude, location.longitude
     lat_delta = distance.distance(kilometers=size).destination((lat, lon), bearing=0).latitude - lat
     lon_delta = distance.distance(kilometers=size).destination((lat, lon), bearing=90).longitude - lon
